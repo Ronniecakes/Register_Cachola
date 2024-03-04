1508d8244e7e3cd154ab405853e641c853b99ad0
Ron Paulo Cachola <cacholaron17@gmail.com>
2024-03-04 09:22:44 +0800

    commit

---
+++ register.js
@@ -0,0 +1,25 @@
+console.log("Hello World.", 43, 409, 2, true, false, ["banana", 43, true]);
+
+console.log(typeof("Hello World."),typeof(34234),
+typeof(["banana"]))
+
+// string = "adsaasd", "ddasdasd", "234"
+// number = 12345, 40.2, 60.4
+// boolean = true, false
+// object = Array [], JSON (...)
+
+let btnRegister = document.querySelector("#btnRegister");
+let txtUsername = document.querySelector("#txtUsername");
+let txtPassword = document.querySelector("#txtPassword");
+let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
+let txtAge = document.querySelector("#txtAge");
+
+
+btnRegister.onclick = function(){
+    register(txtUsername.value, txtPassword.value, txtConfirmPassword.value, txtAge.value);
+}
+
+function register (username, password, confirmPassword, age) {
+    console.log(username, password, confirmPassword, age);
+    // alert("Register button has been clicked")
+}
